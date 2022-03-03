12479f1ccfd7d9b52906bcc05563c4332b497b8e
Edward Thomson <ethomson@edwardthomson.com>
2022-03-02 21:41:26 -0500

    test

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import * as core from "@actions/core"
+
+async function run(): Promise<void> {
+  try {
+    const paths = core.getInput("paths")
+
+    core.debug(paths)
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    } else if (error !== null && error !== undefined) {
+      core.setFailed(error as string)
+    } else {
+      core.setFailed("unknown error")
+    }
+  }
+}
+
+run()
